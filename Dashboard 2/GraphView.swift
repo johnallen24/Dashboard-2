b4b50f9227f2be4689260e3db594d8c57a693360
John Allen <johnallen@Johns-MacBook-Pro-2.local>
2018-05-11 20:43:45 -0400

    Dashboard with transform animaton - makes graphView blurry

---
+++ Dashboard 2/GraphView.swift
@@ -0,0 +1,332 @@
+//
+//  GraphView.swift
+//  Dashboard 2
+//
+//  Created by John Allen on 5/11/18.
+//  Copyright © 2018 jallen.studios. All rights reserved.
+//
+
+import UIKit
+import Charts
+import GTProgressBar
+
+class GraphView: UIView {
+    
+    let chtChart: LineChartView = {
+        let view = LineChartView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.noDataText = "Connect to sensor"
+        view.noDataFont = NSUIFont(name: "HelveticaNeue", size: 18.0)
+        return view
+    }()
+    
+    let topContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let bottomContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        horizontalProgressBar.barBorderColor = colorWithHexString(hexString: "#fe117c")
+        horizontalProgressBar.barFillColor = colorWithHexString(hexString: "#fe117c")
+        horizontalProgressBar.barBackgroundColor = colorWithHexString(hexString: "#fe117c").withAlphaComponent(0.2)
+        verticalProgressBar.barBorderColor = colorWithHexString(hexString: "#fe117c")
+        verticalProgressBar.barFillColor = colorWithHexString(hexString: "#fe117c")
+        verticalProgressBar.barBackgroundColor = colorWithHexString(hexString: "#fe117c").withAlphaComponent(0.2)
+        pieChart.set(colors: colorWithHexString(hexString: "#fe117c"))
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    let titleLabel: UITextView = {
+        let textView = UITextView()
+        textView.text = "Voltage"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 32)
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    
+    let pieChart: KDCircularProgress = {
+        let progress = KDCircularProgress()
+        progress.startAngle = -90
+        progress.progressThickness = 0.4
+        progress.trackThickness = 0.6
+        progress.clockwise = true
+        progress.trackColor = UIColor.lightGray.withAlphaComponent(0.5)
+        progress.gradientRotateSpeed = 2
+        progress.roundedCorners = false
+        progress.glowMode = .noGlow
+        progress.glowAmount = 0.9
+
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        return progress
+    }()
+    
+    let horizontalProgressBar: GTProgressBar = {
+        let progressBar = GTProgressBar()
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        progressBar.backgroundColor = UIColor.white
+        progressBar.progress = 1
+        progressBar.barBorderWidth = 1
+        progressBar.barFillInset = 2
+        progressBar.labelTextColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
+        progressBar.progressLabelInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        progressBar.font = UIFont.boldSystemFont(ofSize: 18)
+        progressBar.labelPosition = GTProgressBarLabelPosition.top
+        progressBar.barMaxHeight = 40 //12
+        progressBar.direction = GTProgressBarDirection.clockwise
+        progressBar.orientation = .horizontal
+        progressBar.displayLabel = false
+        return progressBar
+    }()
+    
+    let verticalProgressBar: GTProgressBar = {
+        let progressBar = GTProgressBar()
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        progressBar.backgroundColor = UIColor.white
+        progressBar.progress = 1
+        progressBar.barBorderWidth = 1
+        progressBar.barFillInset = 2
+        progressBar.labelTextColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
+        progressBar.progressLabelInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        progressBar.font = UIFont.boldSystemFont(ofSize: 18)
+        progressBar.labelPosition = GTProgressBarLabelPosition.top
+        progressBar.barMaxWidth = 30
+        progressBar.direction = GTProgressBarDirection.clockwise
+        progressBar.orientation = .vertical
+        progressBar.displayLabel = false
+        return progressBar
+    }()
+    
+    
+    
+    
+    
+    func setupViews() {
+        
+        self.addSubview(titleLabel)
+        self.addSubview(topContainer)
+        self.addSubview(bottomContainer)
+        topContainer.addSubview(chtChart)
+        topContainer.addSubview(pieChart)
+        bottomContainer.addSubview(horizontalProgressBar)
+        bottomContainer.addSubview(verticalProgressBar)
+        
+        titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        titleLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.12).isActive = true
+        
+        
+        horizontalProgressBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0).isActive = true
+        horizontalProgressBar.heightAnchor.constraint(equalTo: bottomContainer.heightAnchor, multiplier: 0.08).isActive = true
+        horizontalProgressBar.leadingAnchor.constraint(equalTo: bottomContainer.leadingAnchor, constant: 0).isActive = true
+        horizontalProgressBar.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        
+        
+        topContainer.topAnchor.constraint(equalTo: horizontalProgressBar.bottomAnchor, constant: 0).isActive = true
+        topContainer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        topContainer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        topContainer.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.55).isActive = true
+        
+        bottomContainer.topAnchor.constraint(equalTo: topContainer.bottomAnchor, constant: 0).isActive = true
+        bottomContainer.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
+        bottomContainer.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
+        bottomContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        
+        
+        chtChart.topAnchor.constraint(equalTo: topContainer.topAnchor, constant: 0).isActive = true
+        
+        chtChart.bottomAnchor.constraint(equalTo: topContainer.bottomAnchor, constant: 0).isActive = true
+        chtChart.leadingAnchor.constraint(equalTo: topContainer.leadingAnchor, constant: 0).isActive = true
+        chtChart.trailingAnchor.constraint(equalTo: topContainer.trailingAnchor, constant: 0).isActive = true
+        chtChart.backgroundColor = UIColor.white
+        
+        
+        pieChart.topAnchor.constraint(equalTo: bottomContainer.topAnchor, constant: 0).isActive = true
+        pieChart.bottomAnchor.constraint(equalTo: bottomContainer.bottomAnchor, constant: 0).isActive = true
+        pieChart.leadingAnchor.constraint(equalTo: bottomContainer.leadingAnchor, constant:50).isActive = true
+        pieChart.widthAnchor.constraint(equalTo: bottomContainer.heightAnchor, multiplier: 1).isActive = true
+        
+        pieChart.backgroundColor = UIColor.white
+        
+        
+        
+        verticalProgressBar.topAnchor.constraint(equalTo: bottomContainer.topAnchor, constant: 0).isActive = true
+        verticalProgressBar.bottomAnchor.constraint(equalTo: bottomContainer.bottomAnchor, constant: 0).isActive = true
+        verticalProgressBar.leadingAnchor.constraint(equalTo: pieChart.trailingAnchor, constant: 0).isActive = true
+        verticalProgressBar.trailingAnchor.constraint(equalTo: bottomContainer.trailingAnchor, constant: 0).isActive = true
+        //verticalProgressBar.barMaxHeight = bottomContainer.bounds.height
+        
+        
+    }
+    
+    var times: [Double] = [] //holds time at every sensor value
+    var sensorValues: [Double] = [] //holds all values from LoPy
+    var totalXmoved = 0.0
+    var totalValues = 0
+    
+    
+    enum names {
+        case voltage
+        case battery
+    }
+    
+    
+    var graphName: names = .voltage
+    
+    @objc func update() {
+        
+        let number = Double(arc4random_uniform(5)) + Double(Float(arc4random()) / Float(UINT32_MAX)).rounded(toPlaces: 2)
+        
+        if graphName == .voltage {
+            titleLabel.text = "Voltage: \(number)"
+        }
+        else
+        {
+            titleLabel.text = "Battery Level: \(number)%"
+        }
+        
+        
+        let percentage = number / 5.0
+        let angle = percentage * 360
+        pieChart.animate(toAngle: angle, duration: 0.75, completion: nil)
+        horizontalProgressBar.animateTo(progress: CGFloat(percentage))
+        verticalProgressBar.animateTo(progress: CGFloat(percentage))
+        
+        times.append(Double(totalValues))
+        totalValues = 1 + totalValues
+        sensorValues.append(Double(number))
+        updateGraph()
+        
+    }
+    
+    
+    var firstTime = true
+    
+    func updateGraph() {
+        
+        
+        
+        var lineChartEntry  = [ChartDataEntry]()
+        
+        for i in 0..<times.count {
+            
+            let value = ChartDataEntry(x: times[i], y: sensorValues[i])
+            
+            lineChartEntry.append(value)
+        }
+        
+        let chartDataSet = LineChartDataSet(values: lineChartEntry, label: "Voltage")
+        let chartData = LineChartData()
+        
+        chartData.addDataSet(chartDataSet)
+        chartData.setDrawValues(false)
+        chartDataSet.colors = [colorWithHexString(hexString: "#fe117c")]
+        chartDataSet.setCircleColor(colorWithHexString(hexString: "#fe117c"))
+        chartDataSet.circleHoleColor = colorWithHexString(hexString: "#fe117c")
+        //#03C03C
+        chartDataSet.circleRadius = 5
+        //chartDataSet.circleHoleRadius = 3
+        //chartDataSet.circleHoleColor  = UIColor.white
+        
+        chartDataSet.drawCirclesEnabled = true
+        chartDataSet.lineWidth = 2
+        chartDataSet.mode = .horizontalBezier
+        let gradientColors = [colorWithHexString(hexString: "#fe117c").cgColor, UIColor.clear.cgColor] as CFArray
+        let colorLocations: [CGFloat] = [1.0, 0.0]
+        guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("gradient error"); return }
+        chartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
+        chartDataSet.drawFilledEnabled = true
+        
+        chtChart.leftAxis.axisMinimum = 0
+        chtChart.chartDescription?.text = ""
+        chtChart.xAxis.labelPosition = .bottom
+        chtChart.xAxis.drawGridLinesEnabled = false
+        chtChart.chartDescription?.enabled = true
+        chtChart.legend.enabled = true
+        chtChart.rightAxis.enabled = false
+        chtChart.setVisibleXRange(minXRange: 0, maxXRange: 14)
+        chtChart.leftAxis.labelFont = UIFont(name: "AppleSDGothicNeo-Medium", size: 16)!
+        chtChart.xAxis.labelFont = UIFont(name: "AppleSDGothicNeo-Medium", size: 16)!
+        chtChart.leftAxis.drawAxisLineEnabled = false
+        chtChart.legend.enabled = false
+        
+        
+        chtChart.leftAxis.drawGridLinesEnabled = true
+        chtChart.leftAxis.drawLabelsEnabled = true
+        chtChart.leftAxis.gridColor = NSUIColor.gray.withAlphaComponent(0.4)
+        chtChart.data = chartData
+        
+        
+        
+        
+        
+        if (times.count > 14)
+        {
+            //chtChart.moveViewToX(totalXmoved)
+            chtChart.moveViewToAnimated(xValue: totalXmoved, yValue: 0, axis: YAxis.AxisDependency.left, duration: 1, easing: nil)
+            
+            totalXmoved += 1.0
+        }
+        
+        
+    }
+    
+    
+    func colorWithHexString(hexString: String, alpha:CGFloat? = 1.0) -> UIColor {
+        
+        // Convert hex string to an integer
+        let hexint = Int(self.intFromHexString(hexStr: hexString))
+        let red = CGFloat((hexint & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hexint & 0xff00) >> 8) / 255.0
+        let blue = CGFloat((hexint & 0xff) >> 0) / 255.0
+        let alpha = alpha!
+        
+        // Create color object, specifying alpha as well
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return color
+    }
+    
+    func intFromHexString(hexStr: String) -> UInt32 {
+        var hexInt: UInt32 = 0
+        // Create scanner
+        let scanner: Scanner = Scanner(string: hexStr)
+        // Tell scanner to skip the # character
+        scanner.charactersToBeSkipped = NSCharacterSet(charactersIn: "#") as CharacterSet
+        // Scan hex value
+        scanner.scanHexInt32(&hexInt)
+        return hexInt
+    }
+    
+}
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+
+
+
+
+
--- Dashboard 2/ViewController.swift
+++ Dashboard 2/ViewController.swift
@@ -1,25 +1,328 @@
 //
 //  ViewController.swift
-//  Dashboard 2
+//  Dashboard
 //
-//  Created by John Allen on 5/11/18.
+//  Created by John Allen on 5/10/18.
 //  Copyright © 2018 jallen.studios. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIGestureRecognizerDelegate{
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
+   
+    var newViewHeight: CGFloat {
+        get {
+            let tabBarHeight = self.tabBarController?.tabBar.frame.size.height
+            return UIScreen.main.bounds.height - tabBarHeight!
+            
+            
+        }
+    }
+    
+    
+    var newView: UIView!
+        
+        //let rect = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
+    
+    
+    var fullScreenView: UIStackView = {
+        let view = UIStackView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        //view.backgroundColor = UIColor.blue
+        return view
+    }()
+    
+   var visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    
+    
+    var effect:UIVisualEffect!
+    
+    
+    var oldHeight:CGFloat = 0
+    
+    let graphView1: GraphView = {
+        let view = GraphView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.graphName = .battery
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    
+    let graphView2: GraphView = {
+        let view = GraphView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.graphName = .voltage
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    
+    let graphView3: GraphView = {
+        let view = GraphView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+         view.graphName = .battery
+        view.backgroundColor = UIColor.white
+        
+        return view
+    }()
+    
+    let graphView4: GraphView = {
+        let view = GraphView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.graphName = .voltage
+        view.backgroundColor = UIColor.white
+        
+        return view
+    }()
+    
+    let topContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let middleContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var topStackView: UIStackView!
+    var bottomStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        fullScreenView.layer.cornerRadius = 5
+// NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+//
+        
+         view.backgroundColor = colorWithHexString(hexString: "f0f0f0") //f0f0f0
+        //view.setGradientBackground(colorOne: colorWithHexString(hexString: "f0f0f0"), colorTwo: colorWithHexString(hexString: "4d4b4e"))
+        
+        
+        topStackView = UIStackView(arrangedSubviews: [graphView1, graphView2])
+        topStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView = UIStackView(arrangedSubviews: [graphView3, graphView4])
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topStackView.distribution = .fillEqually
+        //topStackView.alignment = .center
+        topStackView.axis = .horizontal
+        topStackView.spacing = 20
+        bottomStackView.distribution = .fillEqually
+        //bottomStackView.alignment = .center
+        bottomStackView.axis = .horizontal
+        bottomStackView.spacing = 20
+        
+        
+        
+        //topStackView.backgroundColor = UIColor.blue
+        //bottomStackView.backgroundColor = UIColor.green
+        topContainer.backgroundColor = colorWithHexString(hexString: "4d4b4e")
+        middleContainer.backgroundColor = colorWithHexString(hexString: "4d4b4e")
+        
+        
+        //view.addSubview(topContainer)
+        view.addSubview(topStackView)
+        //view.addSubview(middleContainer)
+        view.addSubview(bottomStackView)
+        
+     
+        
+        //        NSLayoutConstraint.activate([
+        //            topContainer.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 0),
+        //            topContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        //            topContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        //            topContainer.heightAnchor.constraint(equalToConstant: 40)
+        //        ])
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        tap.delegate = self as! UIGestureRecognizerDelegate
+        graphView1.addGestureRecognizer(tap)
+        
+        
+        let tap2 = UITapGestureRecognizer(target: self, action: #selector(didTapAgain))
+        tap2.delegate = self as! UIGestureRecognizerDelegate
+        fullScreenView.addGestureRecognizer(tap2)
+        
+        NSLayoutConstraint.activate([
+            topStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            topStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            topStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            topStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60)
+            ])
+        
+        //        NSLayoutConstraint.activate([
+        //            middleContainer.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 0),
+        //            middleContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        //            middleContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        //            middleContainer.heightAnchor.constraint(equalToConstant: 40),
+        //            //middleContainer.bottomAnchor.constraint(equalTo: bottomStackView.topAnchor, constant: 0)
+        //            ])
+        
+        
+        NSLayoutConstraint.activate([
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+             bottomStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60),
+             bottomStackView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -40),
+           
+            //bottomStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 40),
+            
+            ])
+        
+        oldHeight = newViewHeight
+        
+        view.layoutIfNeeded()
+        
+    
+        
+        
     }
+    
+    @objc func didTap() {
+        print("hello")
+        graphView1.layer.zPosition = 1;
+        graphView1.layer.shouldRasterize = true
+        graphView1.layer.rasterizationScale = 10
+        view.bringSubview(toFront: graphView1)
+        let originalTransform = graphView1.transform
+        let scaledTransform = originalTransform.scaledBy(x: 2, y: 2)
+        
+        let centerScreen: CGPoint = CGPoint(x: (UIScreen.main.bounds.width/2.0), y: (UIScreen.main.bounds.height - (self.tabBarController?.tabBar.frame.size.height)!)/2.0) //(UIScreen.main.bounds.width/2.0,(UIScreen.main.bounds.height/2) - (self.tabBarController?.tabBar.frame.size.height)!)
+        let graphView1Center = graphView1.center
+        let viewCenter = self.view.center
+        let xtransform = centerScreen.x - graphView1Center.x
+        let ytransform = (centerScreen.y - graphView1Center.y) //- (self.tabBarController?.tabBar.frame.size.height)!
+        let scaledAndTranslatedTransform = scaledTransform.translatedBy(x: 0.5 * xtransform , y: 0.5 * ytransform)
+        UIView.animate(withDuration: 0.5, animations: {
+            self.graphView1.transform = scaledAndTranslatedTransform
+        })
 
+    }
+    
+    @objc func didTapAgain() {
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.fullScreenView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.fullScreenView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+            
+        }) { (success:Bool) in
+            self.fullScreenView.removeFromSuperview()
+        }
+        
+    }
+    
+    
+    @objc func rotated() {
+        
+//        NSLayoutConstraint.deactivate([topStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60),
+//            bottomStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60)])
+        
+        NSLayoutConstraint.deactivate([
+              topStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            topStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            topStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            topStackView.heightAnchor.constraint(equalToConstant: (oldHeight/2) - 60)
+            ])
+        
+        NSLayoutConstraint.deactivate([
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bottomStackView.heightAnchor.constraint(equalToConstant: (oldHeight/2) - 60),
+            bottomStackView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -40)])
+        
+        oldHeight = newViewHeight
+        NSLayoutConstraint.activate([
+            topStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40),
+            topStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            topStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            topStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60)
+            ])
+        
+
+        NSLayoutConstraint.activate([
+            bottomStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+             bottomStackView.heightAnchor.constraint(equalToConstant: (newViewHeight/2) - 60),
+            bottomStackView.bottomAnchor.constraint(equalTo: bottomLayoutGuide.topAnchor, constant: -40),
+            
+            //bottomStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 40),
+           
+            ])
+        
+        print("hey")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func colorWithHexString(hexString: String, alpha:CGFloat? = 1.0) -> UIColor {
+        
+        // Convert hex string to an integer
+        let hexint = Int(self.intFromHexString(hexStr: hexString))
+        let red = CGFloat((hexint & 0xff0000) >> 16) / 255.0
+        let green = CGFloat((hexint & 0xff00) >> 8) / 255.0
+        let blue = CGFloat((hexint & 0xff) >> 0) / 255.0
+        let alpha = alpha!
+        
+        // Create color object, specifying alpha as well
+        let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return color
+    }
+    
+    func intFromHexString(hexStr: String) -> UInt32 {
+        var hexInt: UInt32 = 0
+        // Create scanner
+        let scanner: Scanner = Scanner(string: hexStr)
+        // Tell scanner to skip the # character
+        scanner.charactersToBeSkipped = NSCharacterSet(charactersIn: "#") as CharacterSet
+        // Scan hex value
+        scanner.scanHexInt32(&hexInt)
+        return hexInt
+    }
+    
+    
+    
 }
 
+
+extension UIView {
+    
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+}
+
+extension UIView
+{
+    func copyView<T: UIView>() -> T {
+        return NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as! T
+    }
+}
